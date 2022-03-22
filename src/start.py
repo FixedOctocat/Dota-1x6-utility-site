acae86384b32a4ad3e215338fa2b8bcd912eb242
FixedOctocat <egorfilatov2002@yandex.ru>
2022-03-23 00:49:12 +0300

    First UI

--- src/start.py
+++ src/start.py
@@ -1,2 +1,19 @@
-if __name__=="__main__":
-    pass
+from flask import Flask, request, render_template
+
+import utils.statistic as stat
+
+app = Flask(__name__)
+
+
+@app.route("/leaderboard")
+def leaderboard():
+    data = stat.heroes_leaderbord()
+    
+    hero_names = [hero.name for hero in data]
+    hero_fav = [hero.fav_rating for hero in data]
+    hero_avg = [hero.avg_rating for hero in data]
+
+    return render_template("leaderboard.html", labels=hero_names, hero_fav=hero_fav, hero_avg=hero_avg)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- src/utils/statistic.py
+++ src/utils/statistic.py
@@ -7,10 +7,11 @@ def heroes_leaderbord():
     heroes = [profile["favorHero"] for profile in profiles]
     heroes_games_played = dict(zip(list(heroes), [list(heroes).count(i) for i in list(heroes)]))
 
-    heroes_rating = dict.fromkeys(heroes, 0)
+    heroes_rating = dict.fromkeys(set(heroes), 0)
     for profile in profiles:
         heroes_rating[profile['favorHero']] += int(profile["rating"]/heroes_games_played[profile['favorHero']])
     heroes_rating = dict(sorted(heroes_rating.items(), key=lambda item: item[1]))
-
-    heroes = [Hero(hero, heroes_games_played[hero], heroes_rating[hero]) for hero in heroes]
-    return heroes
+    
+    heroes = [Hero(hero.replace("npc_dota_hero_", ""), heroes_games_played[hero], heroes_rating[hero]) for hero in set(heroes)]
+    sort_heroes = sorted(heroes, key=lambda hero: hero.avg_rating)
+    return sort_heroes
